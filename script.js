40410ece63140585ce535aa2f6a2bcbaefe7412d
Bruno Serpa <brunospenna@gmail.com>
2022-08-22 11:24:00 -0300

    inicio do projeto calculadora criando botão 3 e 9

---
+++ script.js
@@ -0,0 +1,13 @@
+const button3 = document.getElementById('btn_3')
+button3.addEventListener('click', clicouButton3)
+
+function clicouButton3() {
+    window.alert("3")
+}
+
+const button9 = document.getElementById('btn_9')
+button9.addEventListener('click', clicouButton9)
+
+function clicouButton9() {
+    window.alert("9")
+}
