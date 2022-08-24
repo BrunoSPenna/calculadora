b6345fd341d4fbaa5195d14a2edc6358c98845b9
Bruno Serpa <brunospenna@gmail.com>
2022-08-24 14:02:06 -0300

    refactoring numeros

--- script.js
+++ script.js
@@ -3,72 +3,16 @@ let primeiroNumero = ""
 let segundoNumero = ""
 
 
-const button1 = document.getElementById('btn_1')
-button1.addEventListener('click', clicouButton1)
-    
-function clicouButton1() {
-    clickAtBtn("1")
+function bindNumeros() {
+    const numbersContainer = document.getElementById('numeros')  
+    for(const btn of numbersContainer.children) {
+        btn.addEventListener('click', clickNumberBtnEvent)
+    }
 }
 
-const button2 = document.getElementById('btn_2')
-button2.addEventListener('click', clicouButton2)
-    
-function clicouButton2() {
-    clickAtBtn("2")
-}
-const button3 = document.getElementById('btn_3')
-button3.addEventListener('click', clicouButton3)
-    
-function clicouButton3() {
-    clickAtBtn("3")
-}
-const button4 = document.getElementById('btn_4')
-button4.addEventListener('click', clicouButton4)
-    
-function clicouButton4() {
-    clickAtBtn("4")
-}
-const button5 = document.getElementById('btn_5')
-button5.addEventListener('click', clicouButton5)
-    
-function clicouButton5() {
-    clickAtBtn("5")
-}
-const button6 = document.getElementById('btn_6')
-button6.addEventListener('click', clicouButton6)
-    
-function clicouButton6() {
-    clickAtBtn("6")
-}
-
-
-
-const button7 = document.getElementById('btn_7')
-button7.addEventListener('click', clicouButton7)
-
-function clicouButton7() {
-    clickAtBtn("7")
-}
-
-const button8 = document.getElementById('btn_8')
-button8.addEventListener('click', clicouButton8)
-
-function clicouButton8() {
-    clickAtBtn("8")
-}
-
-const button9 = document.getElementById('btn_9')
-button9.addEventListener('click', clicouButton9)
-
-function clicouButton9() {
-    clickAtBtn("9")
-}
-
-const button0 = document.getElementById('btn_0')
-button0.addEventListener('click', clicouButton0)
-
-function clicouButton0() {
-    clickAtBtn("0")
+function clickNumberBtnEvent(event) {
+    const btnText = event.target.innerText
+    clickAtBtn(btnText)
 }
 
 function clickAtBtn(number) {
@@ -76,6 +20,8 @@ function clickAtBtn(number) {
     display.value = displayText + number
 }
     
+
+bindNumeros()
 
 const buttonSoma = document.getElementById('btn_+')
 buttonSoma.addEventListener('click', clicouButtonSoma)
@@ -117,17 +63,6 @@ function clicouButtonClear() {
 }
 
 function toogleBtnSomaStyle(selected) {
-
     buttonSoma.classList.remove(selected ? "btnNotSelected" : "btnSelected")
     buttonSoma.classList.add(selected ? "btnSelected" : "btnNotSelected")
-
-
-    // if (selected) {
-    //     buttonSoma.classList.remove("btnNotSelected")
-    //     buttonSoma.classList.add("btnSelected")
-    // } else {
-    //     buttonSoma.classList.remove("btnSelected")
-    //     buttonSoma.classList.add("btnNotSelected")
-    // }
-
 }
