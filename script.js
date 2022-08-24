62837ca4d9aa2860a41e64dc1e7b36cc1fdcda69
Bruno Serpa <brunospenna@gmail.com>
2022-08-24 13:27:13 -0300

    todos os numeros com adicao

--- script.js
+++ script.js
@@ -1,40 +1,92 @@
 const display = document.getElementById('display')
-display.value = "0"
 let primeiroNumero = ""
 let segundoNumero = ""
 
 
+const button1 = document.getElementById('btn_1')
+button1.addEventListener('click', clicouButton1)
+    
+function clicouButton1() {
+    clickAtBtn("1")
+}
+
+const button2 = document.getElementById('btn_2')
+button2.addEventListener('click', clicouButton2)
+    
+function clicouButton2() {
+    clickAtBtn("2")
+}
 const button3 = document.getElementById('btn_3')
 button3.addEventListener('click', clicouButton3)
-
+    
 function clicouButton3() {
-    let displayText = display.value
-    if(displayText === "0" || primeiroNumero !== "") {
-        display.value = "3"
-    } else {
-        display.value = displayText + "3"
-    }
-       
+    clickAtBtn("3")
+}
+const button4 = document.getElementById('btn_4')
+button4.addEventListener('click', clicouButton4)
+    
+function clicouButton4() {
+    clickAtBtn("4")
+}
+const button5 = document.getElementById('btn_5')
+button5.addEventListener('click', clicouButton5)
+    
+function clicouButton5() {
+    clickAtBtn("5")
+}
+const button6 = document.getElementById('btn_6')
+button6.addEventListener('click', clicouButton6)
+    
+function clicouButton6() {
+    clickAtBtn("6")
+}
+
+
+
+const button7 = document.getElementById('btn_7')
+button7.addEventListener('click', clicouButton7)
+
+function clicouButton7() {
+    clickAtBtn("7")
+}
+
+const button8 = document.getElementById('btn_8')
+button8.addEventListener('click', clicouButton8)
+
+function clicouButton8() {
+    clickAtBtn("8")
 }
 
 const button9 = document.getElementById('btn_9')
 button9.addEventListener('click', clicouButton9)
 
 function clicouButton9() {
-    let displayText = display.value
-    if(displayText === "0" || primeiroNumero !== "") {
-        display.value = "9"
-    } else {
-        display.value = displayText + "9"
-    }
-
+    clickAtBtn("9")
 }
+
+const button0 = document.getElementById('btn_0')
+button0.addEventListener('click', clicouButton0)
+
+function clicouButton0() {
+    clickAtBtn("0")
+}
+
+function clickAtBtn(number) {
+    let displayText = display.value
+    display.value = displayText + number
+}
+    
+
 const buttonSoma = document.getElementById('btn_+')
 buttonSoma.addEventListener('click', clicouButtonSoma)
 
 function clicouButtonSoma() {
    primeiroNumero = display.value
-   buttonSoma.style.backgroundColor = "grey"
+   
+   toogleBtnSomaStyle(true)
+   
+   display.value = ""
+   display.placeholder = primeiroNumero
 }
 
 const buttonIgual = document.getElementById('btn_=')
@@ -43,5 +95,39 @@ buttonIgual.addEventListener('click', clicouButtonIgual)
 function clicouButtonIgual() {
     segundoNumero = display.value
     let resultado = parseInt(primeiroNumero) + parseInt(segundoNumero)
-    display.value = resultado   
+
+    primeiroNumero = resultado
+    display.value = ""
+    display.placeholder = primeiroNumero
+
+    segundoNumero = ""
+}
+
+const buttonClear = document.getElementById('btn_c')
+buttonClear.addEventListener('click', clicouButtonClear)
+
+function clicouButtonClear() {
+    primeiroNumero = ""
+    segundoNumero = ""
+    display.value = ""
+    display.placeholder = "0"
+
+    toogleBtnSomaStyle(false)
+
+}
+
+function toogleBtnSomaStyle(selected) {
+
+    buttonSoma.classList.remove(selected ? "btnNotSelected" : "btnSelected")
+    buttonSoma.classList.add(selected ? "btnSelected" : "btnNotSelected")
+
+
+    // if (selected) {
+    //     buttonSoma.classList.remove("btnNotSelected")
+    //     buttonSoma.classList.add("btnSelected")
+    // } else {
+    //     buttonSoma.classList.remove("btnSelected")
+    //     buttonSoma.classList.add("btnNotSelected")
+    // }
+
 }
